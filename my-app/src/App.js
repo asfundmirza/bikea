5d4261b3709ca0c1cf07d3735b441b3a42cc517e
asfund <asfundmirzaj@gmail.com>
2023-09-06 03:02:14 +0500

    added modal changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import ModalPage from "./components/ModalPage";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <ModalPage />
     </div>
   );
 };
---
+++ my-app/src/components/ModalPage.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ModalPage = () => {
+  return (
+    <div className="flex flex-col md:flex-row mt-4 px-4 py-4">
+      <div className="flex items-center justify-center flex-1">
+        <h1 className="font-semibold text-3xl">Aero Bike</h1>
+      </div>
+      <div className="flex flex-col gap-y-6 flex-1 mt-4">
+        <p>
+          <span className="font-bold">Battery Timing:</span>
+          The Aero Bike is equipped with a powerful lithium-ion battery that
+          offers an impressive 60 kilometers (37 miles) of riding on a single
+          charge.
+        </p>
+        <p>
+          <span className="font-bold">Battery Timing:</span>
+          The Aero Bike is equipped with a powerful lithium-ion battery that
+          offers an impressive 60 kilometers (37 miles) of riding on a single
+          charge.
+        </p>
+        <p>
+          <span className="font-bold">Battery Timing:</span>
+          The Aero Bike is equipped with a powerful lithium-ion battery that
+          offers an impressive 60 kilometers (37 miles) of riding on a single
+          charge.
+        </p>
+        <p>
+          <span className="font-bold">Battery Timing:</span>
+          The Aero Bike is equipped with a powerful lithium-ion battery that
+          offers an impressive 60 kilometers (37 miles) of riding on a single
+          charge.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ModalPage;
